0b0b3e40838a1d63c8c09e6faae5fdb9e7d8b703
Sandbunny <0470hdbutter@gmail.com>
2024-05-15 17:30:08 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-require('dotenv').config()
 const express = require('express')
 const app = express()
-
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.use('/places', require('./controllers/places'))
-
-
-app.get('*', (req, res) => {
-    res.send('<h1>404 Page</h1>')
-})
-
-app.listen(process.env.PORT)
+app.listen(3000)
