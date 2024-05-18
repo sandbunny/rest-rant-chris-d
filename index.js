038cef2bf81aa44550fa0650ebdc3a8747206f1b
Sandbunny <0470hdbutter@gmail.com>
2024-05-17 20:55:23 -0700

    Form data is getting through!

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
-app.use(express.static('public'))
 
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 app.use('/places', require('./controllers/places'))
+
+
 
 app.get('/', (req, res) => {
     res.render('home')
