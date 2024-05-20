f235b71dbdafb388f035b7c17f54d555738165d6
Sandbunny <0470hdbutter@gmail.com>
2024-05-19 18:19:07 -0700

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -50,6 +50,20 @@ router.delete('/:id', (req, res) => {
   }
 })
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
+
 
 
 
