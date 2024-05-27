3f8916a207ae5a251e2cdbb37b4ad43362cd41ed
Sandbunny <0470hdbutter@gmail.com>
2024-05-27 15:27:59 -0700

    Add debug logs and ensure correct configurations

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const app = express()
 
-
+// Middleware and settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
@@ -12,16 +12,34 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+console.log('Starting the application...')
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB')
+}).catch((err) => {
+  console.error('MongoDB connection error:', err)
+  process.exit(1) // Exit the process with failure
+})
+
+// Routes
 app.use('/places', require('./controllers/places'))
 
-mongoose.connect(process.env.MONGO_URI)
-
 app.get('/', (req, res) => {
-    res.render('home')
+  console.log('Serving the home route')
+  res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.render('error404')
+  console.log('Serving the 404 route')
+  res.render('error404')
 })
 
-app.listen(process.env.PORT)
+// Start server
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
